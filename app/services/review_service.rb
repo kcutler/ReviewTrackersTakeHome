ed2a52e79ab170de225449a98ef763bc9d828b95
Kristen <kristen.cutler@stitchfix.com>
2023-06-08 08:11:08 -0600

    Fix indentation for github formatting

--- app/services/review_service.rb
+++ app/services/review_service.rb
@@ -4,68 +4,68 @@ require "open-uri"
 require "json"
 
 class ReviewService
-	def execute(url, options = {})
-		parsed_url = parse_url(url)
-		validate_lending_tree_url(parsed_url)
-		review_data = extract_review_data(parsed_url)
+  def execute(url, options = {})
+    parsed_url = parse_url(url)
+    validate_lending_tree_url(parsed_url)
+    review_data = extract_review_data(parsed_url)
 
-		sort_review_data(review_data, options[:sort]) if options.has_key?(:sort)
-		save_review_data(review_data) if options.has_key?(:save)
+    sort_review_data(review_data, options[:sort]) if options.has_key?(:sort)
+    save_review_data(review_data) if options.has_key?(:save)
 
-		review_data
-	end
+    review_data
+  end
 
-	private
+  private
 
-	def extract_review_data(parsed_url)
-		html = Nokogiri::HTML(URI.open(parsed_url))
+  def extract_review_data(parsed_url)
+    html = Nokogiri::HTML(URI.open(parsed_url))
 
-		html.css(".mainReviews").map do |review|
-			{
-				title: review.css(".reviewTitle").text.strip,
-				content: review.css(".reviewText").text.strip,
-				author: review.css(".consumerName").text.split(" ")[0],
-				rating: review.css(".numRec").text.split(" ")[0][1].to_i,
-				date: DateTime.parse(review.css(".consumerReviewDate").text.split(" ")[-2..-1].join(" "))
-			}
-		end
+    html.css(".mainReviews").map do |review|
+      {
+        title: review.css(".reviewTitle").text.strip,
+        content: review.css(".reviewText").text.strip,
+        author: review.css(".consumerName").text.split(" ")[0],
+        rating: review.css(".numRec").text.split(" ")[0][1].to_i,
+        date: DateTime.parse(review.css(".consumerReviewDate").text.split(" ")[-2..-1].join(" "))
+      }
+    end
 
-	rescue OpenURI::HTTPError => e
-		raise StandardError, "Could not open URL due to #{e.message}"
-	rescue Nokogiri::SyntaxError
-		raise StandardError, "Could not parse data"
-	end
+  rescue OpenURI::HTTPError => e
+    raise StandardError, "Could not open URL due to #{e.message}"
+  rescue Nokogiri::SyntaxError
+    raise StandardError, "Could not parse data"
+  end
 
-	def parse_url(url)
-		URI.parse(url)
+  def parse_url(url)
+    URI.parse(url)
 
-	rescue URI::InvalidURIError
-		raise StandardError, "URL #{url} is not valid"
-	end
+  rescue URI::InvalidURIError
+    raise StandardError, "URL #{url} is not valid"
+  end
 
-	def validate_lending_tree_url(parsed_url)
-		valid_host = "www.lendingtree.com"
-		valid_path = %r{^/reviews/business/[A-Za-z-]+/\d+}
+  def validate_lending_tree_url(parsed_url)
+    valid_host = "www.lendingtree.com"
+    valid_path = %r{^/reviews/business/[A-Za-z-]+/\d+}
 
-		unless parsed_url && valid_host.include?(parsed_url.host) && parsed_url.path =~ valid_path
-			raise StandardError, "URL #{parsed_url} is not a LendingTree Business Review" 
-		end
-	end
+    unless parsed_url && valid_host.include?(parsed_url.host) && parsed_url.path =~ valid_path
+      raise StandardError, "URL #{parsed_url} is not a LendingTree Business Review" 
+    end
+  end
 
-	def save_review_data(review_data)
-		File.open("reviews.json", "w") do |file|
+  def save_review_data(review_data)
+    File.open("reviews.json", "w") do |file|
       file.write(JSON.pretty_generate(review_data))
     end
 
   rescue IOError => e
-  	raise StandardError, "Could not save review data to file due to #{e.message}"
-	end
+    raise StandardError, "Could not save review data to file due to #{e.message}"
+  end
 
-	def sort_review_data(review_data, field)
-		acceptable_fields = ["title", "author", "rating", "date"]
+  def sort_review_data(review_data, field)
+    acceptable_fields = ["title", "author", "rating", "date"]
 
-		raise StandardError, "Invalid sort option: #{field}" unless acceptable_fields.include?(field)
+    raise StandardError, "Invalid sort option: #{field}" unless acceptable_fields.include?(field)
 
-		review_data.sort_by! { |review| review[field.to_sym] }
-	end
+    review_data.sort_by! { |review| review[field.to_sym] }
+  end
 end
--- spec/services/review_service_spec.rb
+++ spec/services/review_service_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
 RSpec.describe "ReviewService" do
-	let(:subject) { ReviewService.new }
-	let(:review_url) { "https://www.lendingtree.com/reviews/business/name-of-business/12345678" }
-	let(:mortgage_url) { "https://www.lendingtree.com/reviews/mortgage/name-of-morgage/98765432" }
-	let(:no_name_url)  { "https://www.lendingtree.com/reviews/business" }
-	let(:other_url) { "https://anotherwebsite.com"}
-	let(:faulty_url) { "https://badurl.com/articles\\/...." }
-	let(:review_data) do
-		[
+  let(:subject) { ReviewService.new }
+  let(:review_url) { "https://www.lendingtree.com/reviews/business/name-of-business/12345678" }
+  let(:mortgage_url) { "https://www.lendingtree.com/reviews/mortgage/name-of-morgage/98765432" }
+  let(:no_name_url)  { "https://www.lendingtree.com/reviews/business" }
+  let(:other_url) { "https://anotherwebsite.com"}
+  let(:faulty_url) { "https://badurl.com/articles\\/...." }
+  let(:review_data) do
+    [
       { title: "Review of a company", content: "Some content", author: "Author 11", rating: 4, date: DateTime.parse("January 2021") },
       { title: "My review", content: "Other content", author: "Author 22", rating: 2, date: DateTime.parse("November 2020") },
       { title: "Best company ever", content: "Additional content", author: "Author 44", rating: 5, date: DateTime.parse("July 1998") }
@@ -16,113 +16,113 @@ RSpec.describe "ReviewService" do
   end
 
   describe "#execute" do
-		it "validates the URL is a Lending Tree business" do
-			expect { subject.execute(review_url) }.not_to raise_error
-		end
+    it "validates the URL is a Lending Tree business" do
+      expect { subject.execute(review_url) }.not_to raise_error
+    end
 
-		it "raises an error when the URL is from Lending Tree and has no business name" do
-			expect { subject.execute(no_name_url) }.to raise_error(StandardError, "URL #{no_name_url} is not a LendingTree Business Review")
-		end
+    it "raises an error when the URL is from Lending Tree and has no business name" do
+      expect { subject.execute(no_name_url) }.to raise_error(StandardError, "URL #{no_name_url} is not a LendingTree Business Review")
+    end
 
-		it "raises an error when the URL is from another Lending Tree path" do
-			expect { subject.execute(mortgage_url) }.to raise_error(StandardError, "URL #{mortgage_url} is not a LendingTree Business Review")
-		end
+    it "raises an error when the URL is from another Lending Tree path" do
+      expect { subject.execute(mortgage_url) }.to raise_error(StandardError, "URL #{mortgage_url} is not a LendingTree Business Review")
+    end
 
-		it "raises an error when the URL is from another website" do
-			expect { subject.execute(other_url) }.to raise_error(StandardError, "URL #{other_url} is not a LendingTree Business Review")
-		end
+    it "raises an error when the URL is from another website" do
+      expect { subject.execute(other_url) }.to raise_error(StandardError, "URL #{other_url} is not a LendingTree Business Review")
+    end
 
-		it "raises an error when the URL format is invalid" do
-			allow(URI).to receive(:parse).with(faulty_url).and_raise(URI::InvalidURIError)
+    it "raises an error when the URL format is invalid" do
+      allow(URI).to receive(:parse).with(faulty_url).and_raise(URI::InvalidURIError)
 
-			expect { subject.execute(faulty_url) }.to raise_error(StandardError, "URL #{faulty_url} is not valid")
-		end
+      expect { subject.execute(faulty_url) }.to raise_error(StandardError, "URL #{faulty_url} is not valid")
+    end
 
-		context "when the URL is valid" do
-			it "returns an array of review data" do
-	      allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_return(double(map: review_data))
+    context "when the URL is valid" do
+      it "returns an array of review data" do
+        allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_return(double(map: review_data))
 
-				expect(subject.execute(review_url)).to eq(review_data)
-			end
+        expect(subject.execute(review_url)).to eq(review_data)
+      end
 
-			it "raises an error when the URL cannot be opened" do
-				allow(URI).to receive(:open).and_raise(OpenURI::HTTPError.new("503", "Service Unavailable"))
+      it "raises an error when the URL cannot be opened" do
+        allow(URI).to receive(:open).and_raise(OpenURI::HTTPError.new("503", "Service Unavailable"))
 
-	      expect { subject.execute(review_url) }.to raise_error(StandardError, "Could not open URL due to 503")
-			end
+        expect { subject.execute(review_url) }.to raise_error(StandardError, "Could not open URL due to 503")
+      end
 
-			it "raises a StandardError when error processing data" do
-				allow(URI).to receive(:open)
-	      allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_raise(Nokogiri::SyntaxError)
+      it "raises a StandardError when error processing data" do
+        allow(URI).to receive(:open)
+        allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_raise(Nokogiri::SyntaxError)
 
-	      expect { subject.execute(review_url) }.to raise_error(StandardError, "Could not parse data")
-	    end
-		end
+        expect { subject.execute(review_url) }.to raise_error(StandardError, "Could not parse data")
+      end
+    end
 
-		context "when a sort option is passed" do
-			before do
-				allow(URI).to receive(:open)
-				allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_return(double(map: review_data))
-			end
+    context "when a sort option is passed" do
+      before do
+        allow(URI).to receive(:open)
+        allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_return(double(map: review_data))
+      end
 
-			it "sorts the review data by title " do
-	      sorted_by_title = [
-					{ title: "Best company ever", content: "Additional content", author: "Author 44", rating: 5, date: DateTime.parse("July 1998") },
-					{ title: "My review", content: "Other content", author: "Author 22", rating: 2, date: DateTime.parse("November 2020") },
-					{ title: "Review of a company", content: "Some content", author: "Author 11", rating: 4, date: DateTime.parse("January 2021") }
-				]
+      it "sorts the review data by title " do
+        sorted_by_title = [
+          { title: "Best company ever", content: "Additional content", author: "Author 44", rating: 5, date: DateTime.parse("July 1998") },
+          { title: "My review", content: "Other content", author: "Author 22", rating: 2, date: DateTime.parse("November 2020") },
+          { title: "Review of a company", content: "Some content", author: "Author 11", rating: 4, date: DateTime.parse("January 2021") }
+        ]
 
-				expect(subject.execute(review_url, { sort: "title" })).to eq(sorted_by_title)
-			end
+        expect(subject.execute(review_url, { sort: "title" })).to eq(sorted_by_title)
+      end
 
-			it "sorts the review data by rating" do
-	      sorted_by_rating = [
-					{ title: "My review", content: "Other content", author: "Author 22", rating: 2, date: DateTime.parse("November 2020") },
-					{ title: "Review of a company", content: "Some content", author: "Author 11", rating: 4, date: DateTime.parse("January 2021") },
-					{ title: "Best company ever", content: "Additional content", author: "Author 44", rating: 5, date: DateTime.parse("July 1998") }
-				]
+      it "sorts the review data by rating" do
+        sorted_by_rating = [
+          { title: "My review", content: "Other content", author: "Author 22", rating: 2, date: DateTime.parse("November 2020") },
+          { title: "Review of a company", content: "Some content", author: "Author 11", rating: 4, date: DateTime.parse("January 2021") },
+          { title: "Best company ever", content: "Additional content", author: "Author 44", rating: 5, date: DateTime.parse("July 1998") }
+        ]
 
-				expect(subject.execute(review_url, { sort: "rating" })).to eq(sorted_by_rating)
-			end
+        expect(subject.execute(review_url, { sort: "rating" })).to eq(sorted_by_rating)
+      end
 
-			it "sorts the review data by date" do
-	      sorted_by_date = [
-					{ title: "Best company ever", content: "Additional content", author: "Author 44", rating: 5, date: DateTime.parse("July 1998") },
-					{ title: "My review", content: "Other content", author: "Author 22", rating: 2, date: DateTime.parse("November 2020") },
-					{ title: "Review of a company", content: "Some content", author: "Author 11", rating: 4, date: DateTime.parse("January 2021") }
-				]
+      it "sorts the review data by date" do
+        sorted_by_date = [
+          { title: "Best company ever", content: "Additional content", author: "Author 44", rating: 5, date: DateTime.parse("July 1998") },
+          { title: "My review", content: "Other content", author: "Author 22", rating: 2, date: DateTime.parse("November 2020") },
+          { title: "Review of a company", content: "Some content", author: "Author 11", rating: 4, date: DateTime.parse("January 2021") }
+        ]
 
-				expect(subject.execute(review_url, { sort: "date" })).to eq(sorted_by_date)
-			end
+        expect(subject.execute(review_url, { sort: "date" })).to eq(sorted_by_date)
+      end
 
-			it "raises an error with an invalid sort field" do
-	      expect { subject.execute(review_url, { sort: "content" }) }.to raise_error(StandardError, "Invalid sort option: content")
-			end
-		end
+      it "raises an error with an invalid sort field" do
+        expect { subject.execute(review_url, { sort: "content" }) }.to raise_error(StandardError, "Invalid sort option: content")
+      end
+    end
 
-		context "when a save option is passed" do
-			before do
-				allow(URI).to receive(:open)
-				allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_return(double(map: review_data))
-			end
+    context "when a save option is passed" do
+      before do
+        allow(URI).to receive(:open)
+        allow_any_instance_of(Nokogiri::HTML::Document).to receive(:css).and_return(double(map: review_data))
+      end
 
-			it "saves the review data to a file" do
-				subject.execute(review_url, { save: true })
+      it "saves the review data to a file" do
+        subject.execute(review_url, { save: true })
 
-				expect(File.exist?("reviews.json")).to be(true)
+        expect(File.exist?("reviews.json")).to be(true)
 
-				saved_data = File.read("reviews.json")
+        saved_data = File.read("reviews.json")
 
-  			expect(saved_data).to eq(JSON.pretty_generate(review_data))
+        expect(saved_data).to eq(JSON.pretty_generate(review_data))
 
-				FileUtils.rm("reviews.json")
-			end
+        FileUtils.rm("reviews.json")
+      end
 
-			it "raises an error when it cannot save the review data" do
-				allow(File).to receive(:open).and_raise(IOError)
+      it "raises an error when it cannot save the review data" do
+        allow(File).to receive(:open).and_raise(IOError)
 
-				expect { subject.execute(review_url, { save: true }) }.to raise_error(StandardError, "Could not save review data to file due to IOError")
-			end
-		end
-	end
+        expect { subject.execute(review_url, { save: true }) }.to raise_error(StandardError, "Could not save review data to file due to IOError")
+      end
+    end
+  end
 end
